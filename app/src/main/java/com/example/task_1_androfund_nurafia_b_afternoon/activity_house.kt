d7501512918c827d774e306c841a4cb778685783
Nurafia <nur.24afia@gmail.com>
2023-11-11 18:49:24 +0900

    tambah file

---
+++ app/src/main/java/com/example/task_1_androfund_nurafia_b_afternoon/activity_house.kt
@@ -0,0 +1,11 @@
+package com.example.task_1_androfund_nurafia_b_afternoon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_house : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_house)
+    }
+}
